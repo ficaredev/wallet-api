3474fc69357d168ceab7a166fc82299745b2fec6
Victor Tesoura Júnior <txsoura@yahoo.com>
2022-05-24 01:09:25 -0300

    feat: create invalid attribute exception

---
+++ src/main/java/com/msc/wallet/exceptions/InvalidAttributeException.java
@@ -0,0 +1,7 @@
+package com.msc.wallet.exceptions;
+
+public class InvalidAttributeException extends RuntimeException {
+    public InvalidAttributeException(String message) {
+        super(message);
+    }
+}
