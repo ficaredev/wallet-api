fa730a0f99cf221cb86261e5db34b7ab99f6cc33
Victor Tesoura Júnior <txsoura@yahoo.com>
2022-05-28 20:54:12 -0300

    refactor: get wallet enum from faker

--- src/main/java/com/msc/wallet/database/factories/WalletFactory.java
+++ src/main/java/com/msc/wallet/database/factories/WalletFactory.java
@@ -5,7 +5,6 @@ import com.msc.wallet.enums.WalletOwner;
 import com.msc.wallet.models.Wallet;
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
 import java.util.UUID;
 
 @Component
@@ -13,8 +12,6 @@ public class WalletFactory {
     public static Faker faker = new Faker();
 
     public Wallet run() {
-        int owner = new Random().nextInt(WalletOwner.values().length);
-
-        return new Wallet(UUID.randomUUID(), faker.bool().bool(), WalletOwner.values()[owner], (int) faker.number().randomNumber());
+        return new Wallet(UUID.randomUUID(), faker.bool().bool(), faker.options().option(WalletOwner.values()), (int) faker.number().randomNumber());
     }
 }
